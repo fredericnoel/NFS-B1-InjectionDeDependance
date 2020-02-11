e3cdb752380177af9a32083421abc4e0ba4c89ff
Frédéric NOËL <contact@fredericnoel.com>
2020-02-11 15:36:49 +0100

    Utilisation de l'espace de nommage \classes\Adresse;
    Test d'un objet de la classe Adresse;

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
+use \classes\Adresse;
+
+$testAdresse = new Adresse('42', 'Michel Legrand', '76210', 'Bolbec', 'Enkulistan');
+var_dump($testAdresse);
